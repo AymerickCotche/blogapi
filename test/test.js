7b5f08d1df1e6450fb5114c8b1336cc817c774ad
AymerickCotche <aymerick.cotche@hotmail.fr>
2024-05-07 12:37:22 +0400

    lint ok

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const { expect} = require("chai")
-const request = require('supertest');
+const request = require('supertest')
 const app = require('../server');
 
 describe('TEST FOR CATEGORIES', () => {
